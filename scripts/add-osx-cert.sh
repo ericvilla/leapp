724f7963399fb7b239d86f59dbde79e7c7e0d775
Eric Villa <eric.villa91@gmail.com>
2020-12-10 23:19:47 +0100

    Changed add-osx-cert.sh script

--- scripts/add-osx-cert.sh
+++ scripts/add-osx-cert.sh
@@ -3,23 +3,24 @@
 KEY_CHAIN=build.keychain
 CERTIFICATE_P12=certificate.p12
 
-# Recreate the certificate from the secure environment variable
+echo "Recreate the certificate from the secure environment variable"
 echo $CERTIFICATE_OSX_P12 | base64 --decode > $CERTIFICATE_P12
+cat $CERTIFICATE_P12
 
 echo "security create-keychain"
-security create-keychain -p travis $KEY_CHAIN
+security create-keychain -p jenkins $KEY_CHAIN
 echo "security list-keychains"
 security list-keychains -s login.keychain build.keychain
 echo "security default-keychain"
 security default-keychain -s $KEY_CHAIN
 echo "security unlock-keychain"
-security unlock-keychain -p travis $KEY_CHAIN
+security unlock-keychain -p jenkins $KEY_CHAIN
 echo "security import"
 security import $CERTIFICATE_P12 -k $KEY_CHAIN -P "" -T /usr/bin/codesign;
 echo "security find-identity"
 security find-identity -v
 echo "security set-key-partition-list"
-security set-key-partition-list -S apple-tool:,apple:,codesign:, -s -k travis $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple:,codesign:, -s -k jenkins $KEY_CHAIN
 
 # Remove certs
 rm -fr *.p12
